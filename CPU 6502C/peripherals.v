5f94137f3fb8473aeafd05fb0aec2e6fba8c4a43
Benjamin Hong <bhong@andrew.cmu.edu>
2014-09-30 19:28:58 -0400

    added functionality for rewriting memory, timing unconfirmed

--- CPU 6502C/peripherals.v
+++ CPU 6502C/peripherals.v
@@ -12,6 +12,7 @@
 /* Changelog:
 	15 Sep 2014,  0033hrs: added memory module (chue)
   29 Sep 2014,  1948hrs: modified memory module to become linear (ben)
+  30 Sep 2014,  1927hrs: amended memory module to be writable (ben)
 */
 
 
@@ -28,6 +29,7 @@ module memory256x256 (clock, enable, we_L, re_L,address,
 	
 	// [7:0] addL, addH;
 	//assign {addH,addL} = address;
+  reg [7:0] data_reg;
 	
 	reg [7:0] mem [0:65535]; //65kb of linear memory declared
   
@@ -39,11 +41,19 @@ module memory256x256 (clock, enable, we_L, re_L,address,
 	// memory256 memRows[255:0](.clock(clock), .enable(enable), .we_L(we_L), .re_L(re_L),
 						// .address(addL), .data(dataCol);
             
-	assign data = (enable & ~re_L) ? mem[address] : 8'bzzzzzzzz;
+	assign data = (enable & ~re_L) ? data_reg : 8'bzzzzzzzz;
 	always @(posedge clock) begin
 		if (enable & ~we_L) begin
-      $display("we're storing data at %h", address);
+      // $display("we're storing data at %h", address);
 			mem[address] <= data;
+    end
+    else if (~re_L) begin
+      // $display("we're reading data at %h", address);
+      data_reg <= mem[address]; //not sure if this screws up the timing for memory being accessible not.
+    end
+    else begin
+      $display("something's wrong...");
+      data_reg <= 8'bxxxxxxxx;
     end
 	end
 
--- CPU 6502C/testbench.v
+++ CPU 6502C/testbench.v
@@ -34,12 +34,12 @@ module testMem256x256;
 
   reg clock, enable, we_L, re_L;
   reg [15:0] address;
-  //reg [7:0] data;
+  reg [7:0] data_reg;
   
   wire [7:0] data;
   
   always begin
-    #10 clock = ~clock;
+    forever #10 clock = ~clock;
   end
   
   memory256x256 mem256x256_module(.clock(clock), .enable(enable), .we_L(we_L), .re_L(re_L), .address(address), .data(data));
@@ -51,15 +51,19 @@ module testMem256x256;
     end
   endtask
   
+  assign data = (enable & ~we_L & re_L) ? data_reg : 8'bzzzzzzzz; //if its time to write, then data should have data_reg, otherwise it should be disconnected
+  
   
   initial begin
+    clock = 1'b0;
+    @(posedge clock);
     
     enable = 1'b1;
     we_L = 1'b1;
     re_L = 1'b0;
     
     #50;
-    $display("printing from memory...");
+    $display("initial printing from memory...");
     $display("=======================");
     for (address = 16'd0; address < 16'hFFFF; address = address + 16'd1) begin
       #50;
@@ -69,9 +73,28 @@ module testMem256x256;
     printMem;
     $display("=======================");
     $display("done printing from memory...");
+    
+    
+    $display("amending memory...");
+    enable = 1'b1;
+    we_L = 1'b0;
+    re_L = 1'b1;
+    address = 16'd0;
+    data_reg = 8'hBE;
+    #50;
+    
+    $display("done amending memory...");
+    $display("printing from amended memory...");
+    enable = 1'b1;
+    we_L = 1'b1;
+    re_L = 1'b0;
+    address = 16'd0;
+    #50;
+    printMem;
+    
+    $display("done printing amended memory...");
+  
     $finish;
-  
-  
   
   end
   
