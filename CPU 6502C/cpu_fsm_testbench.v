9b93d0535c916dcaae0faf8621f750b62bdf8c5a
Benjamin Hong <bhong@andrew.cmu.edu>
2014-10-01 12:21:54 -0400

    edited cpu_fsm_testbench.v and cpu_testbench.v

--- CPU 6502C/cpu_fsm_testbench.v
+++ CPU 6502C/cpu_fsm_testbench.v
@@ -8,53 +8,69 @@
 
 module testCPU_FSM;
 
-  parameter NUM_VECTORS=10;
+  //This number needs to change according to how many lines there are in fsm_test_vectors.vm
+  parameter NUM_VECTORS=30;
   
-  reg phi1, phi2, nmi, irq, rst, RDY;
-  reg [7:0] statusReg, opcodeIn; //what is this status reg?
+  reg phi1, nmi, irq, rst, RDY;
+  reg [7:0] statusReg, opcodeIn; 
+  wire phi2;
+  
   
   reg [2:0] numCycles;
   
-  reg [18:0] vectors [NUM_VECTORS-1:0];
-  reg [18:0] vec;
+  reg [19:0] vectors [NUM_VECTORS-1:0];
+  reg [19:0] vec; 
+  //20 bits per test vector, (nmi, irq, rst, RDY, opcodeIn, statusReg)
   
   integer i, j;
   
+  
   wire [62:0] controlSigs;
   wire SYNC;
-  wire setBflag, clrBflag;
   
-  plaFSM plaFSM_mod(.phi1(phi1), .phi2(phi2), .nmi(nmi), .rst(rst), .RDY(RDY), .statusReg(statusReg), .opcodeIn(opcodeIn), .controlSigs(controlSigs), .SYNC(SYNC), .setBflag(setBflag), .clrBflag(clrBflag));
+  //Setting up the clock signals
+  assign phi2 = ~phi1;
   
-  //need to keep flipping two clocks? off by half?
+  always begin
+    #10 phi1 = ~phi1; //check after phi1 goes up @(posedge phi1)
+  end
   
-  //need to figure out how many times to clock the system before the results are known
+  
+  
+  plaFSM plaFSM_mod(.phi1(phi1), .phi2(phi2), .nmi(nmi), .irq(irq), .rst(rst), .RDY(RDY), .opcodeIn(opcodeIn), .statusReg(statusReg), .controlSigs(controlSigs), .SYNC(SYNC));
+  
+  //need to keep flipping two clocks? off by half? Ans: no, just flip phi1 and get phi2 to be ~phi1
+  
+  //on every clock we should display the produced controlSignals
   //put into vm file, 8 bit statusReg + 8 bit opcode + 3 bits to tell the number of required cycles
   //use task to cycle the appropriate number of times, then display the controlSigs, SYNC, and others
   
-  
+  //20 bits per test vector, (nmi, irq, rst, RDY, opcodeIn, statusReg)
   task run_vector;
-  input [7:0] A, B;
-  input [2:0] C;
+  input A, B, C, D;
+  input [7:0] E, F;
   begin
-    statusReg = A;
-    opcodeIn = B;
-    numCycles = C;
+    nmi = A;
+    irq = B;
+    rst = C;
+    RDY = D;
+    opcodeIn = E;
+    statusReg = F;
+    // numCycles = C;
     //clock the number of cycles, after this, the result should be ready on controlSigs already.
     //@(posedge clock) x 30
     
     $display("======================");
     
-    $display("\tstatusReg is: \t%b, \topcodeIn is: \t%b, \tnumCycles is: \t%d", statusReg, opcodeIn, numCycles);
+    $display("\tstatusReg is: \t%b, \topcodeIn is: \t%b, \tnmi is: \t%b, \tirq is: \t%b, \trst is: \t%b, \tRDY is: \t%b", statusReg, opcodeIn, nmi, irq, rst, RDY);
     
-    
-    for (j = 3'd0; j < numCycles; j=j+1) begin
+    @(posedge phi1);
+    /* for (j = 3'd0; j < numCycles; j=j+1) begin
       @(posedge phi1);
-      @(posedge phi2);
-    end
+     end*/
     
     
-    $display("\tcontrolSigs: \t%b, \tSYNC: \t%b, \tsetBflag: \t%b, \tclrBflag: \t%b", controlSigs, SYNC, setBflag, clrBflag);
+    $display("\topcodeIn: \t%h, \tTstate: \t%h, \tcontrolSigs: \t%b, \tSYNC: \t%b", opcodeIn, plaFSM_mod.curr_T, controlSigs, SYNC);
     
     $display("======================");
     
@@ -71,8 +87,8 @@ module testCPU_FSM;
   $readmemh("fsm_test_vectors.vm", vectors);
   for (i=0; i<NUM_VECTORS; i=i+1) begin
     vec = vectors[i];
-    run_vector(vec[18:11], vec[10:3], vec[2:0]);
-    //run_vector(statusReg, opcodeIn, numCycles);
+    run_vector(vec[19], vec[18], vec[17], vec[16], vec[15:8], vec[7:0]);
+    //run_vector(4 bits of signals, statusReg, opcodeIn);
   end
   
   $finish;
---
+++ CPU 6502C/cpu_testbench.v
@@ -0,0 +1,48 @@
+/*  Test module for top level CPU 6502C 
+ *  Created:        1 Oct 2014, 1058 hrs (bhong)
+ *  Last Modified:  1 Oct 2014, 1058 hrs
+
+ Designed to test module "top_6502C", located in "6502C_top.v"
+
+*/
+
+module testCPU;
+  
+  reg RDY, IRQ_L, NMI_L, RES_L, SO, phi0_in;
+  reg [7:0] DBin;
+  
+  wire phi1_out, SYNC, phi2_out, RW;
+  wire [15:0] AB; //Address Bus Low and High
+  
+  wire [7:0] DB; //Data Bus
+  
+  top_6502C top_6502C_module(.RDY(RDY), .IRQ_L(IRQ_L), .NMI_L(NMI_L), .RES_L(RES_L), .SO(SO), .phi0_in(phi0_in), .DB(DB), .phi1_out(phi1_out), .SYNC(SYNC), .AB(AB), .phi2_out(phi2_out), .RW(RW));
+  
+  /* High level description:  
+   *  We want to generate an opcode on the databus, provide it to the CPU, and have it return a certain checkable value (e.g. control signals and Tstates (which we will have to hook out)). 
+   Other components we want to observe: 
+    - Address Bus (AB) high and low
+    - Side Bus (SB) and Data Bus (DB)
+    - X and Y registers
+    - A and B registers
+    - Accumulator registers
+    - Status Register
+    - Adder Hold Register
+    - Decimal Adjust
+    - 
+
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
