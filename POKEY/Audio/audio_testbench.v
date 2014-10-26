c6161fd968bf1cb5104bcc158615119fb4820aba
chuehsien <chuehsig@andrew.cmu.edu>
2014-10-26 02:00:05 -0400

    pokeyaudio round1

---
+++ POKEY/Audio/audio_testbench.v
@@ -0,0 +1,131 @@
+`include "peripherals.v"
+`include "pokeyaudio.v"
+module pokey_top(USER_CLK,GPIO_SW_C,GPIO_DIP_SW1,GPIO_DIP_SW2,GPIO_DIP_SW3,GPIO_DIP_SW4,GPIO_DIP_SW5,GPIO_DIP_SW6,GPIO_DIP_SW7,GPIO_DIP_SW8,
+               HDR1_2,HDR1_4,HDR1_6,HDR1_8,HDR1_10);
+    input USER_CLK,GPIO_SW_C,GPIO_DIP_SW1,GPIO_DIP_SW2,GPIO_DIP_SW3,GPIO_DIP_SW4,GPIO_DIP_SW5,GPIO_DIP_SW6,GPIO_DIP_SW7,GPIO_DIP_SW8;
+    output HDR1_2,HDR1_4,HDR1_6,HDR1_8,HDR1_10;
+
+    (* clock_signal = "yes" *) wire clk179,clk64,clk16,clk2;
+    clockDivider #(28) out179(USER_CLK,clk179);
+    clockDivider #(1562) out64(USER_CLK,clk64);
+    clockDivider #(6250) out16(USER_CLK,clk16);
+    clockDivider #(2) out2(USER_CLK,clk2);
+ 
+    wire init_L;
+    wire [7:0] AUDF1,AUDF2,AUDF3,AUDF4;
+    wire [7:0] AUDC1,AUDC2,AUDC3,AUDC4,AUDCTL;
+
+    assign init_L = ~GPIO_SW_C;
+    assign AUDF1 = 8'b11010101; //to create 300Hz tone
+    assign AUDF2 = 8'd1;
+    assign AUDF3 = 8'd1;
+    assign AUDF4 = 8'd1;
+    
+    wire [2:0] distort;
+    wire volO;
+    wire [3:0] vol;
+    assign AUDC1 = {distort,volO,vol};
+    assign AUDC2 = 8'd0;
+    assign AUDC3 = 8'd0;
+    assign AUDC4 = 8'd0;
+    
+    assign AUDCTL = 8'd0;
+    
+    assign vol = {GPIO_DIP_SW1,GPIO_DIP_SW2,GPIO_DIP_SW3,GPIO_DIP_SW4};
+    assign distort = {GPIO_DIP_SW6,GPIO_DIP_SW7,GPIO_DIP_SW8};
+    assign volO = GPIO_DIP_SW5;
+
+    
+    wire audio1,audio2,audio3,audio4;
+    wire [3:0] vol1,vol2,vol3,vol4;
+    wire chn1baseT;
+    pokeyaudio test(chn1baseA,init_L,clk179,clk64,clk16,AUDF1,AUDF2,AUDF3,AUDF4,
+                    AUDC1,AUDC2,AUDC3,AUDC4,AUDCTL,
+                    audio1,audio2,audio3,audio4,vol1,vol2,vol3,vol4);
+                    
+                
+    assign HDR1_2 = audio1;
+    assign {HDR1_4,HDR1_6,HDR1_8,HDR1_10} = vol1;
+    
+//=======================ILA/ICON stuff=======================//
+    
+    
+    wire chipClk_b0,chipClk;
+    clockone2048 test11(USER_CLK,chipClk_b0);
+    clockone256  test12(chipClk_b0,chipClk);
+
+    wire [7:0] TRIG0,TRIG1;
+    wire[7:0] TRIG2,TRIG3,TRIG4,TRIG5,TRIG6,TRIG7,TRIG8,TRIG9,TRIG10,TRIG11,TRIG12,TRIG13,TRIG14,TRIG15;
+    wire [35 : 0] CONTROL0,CONTROL1;
+    
+    chipscope_icon icon(
+    .CONTROL0(CONTROL0));
+               
+    
+    chipscope_ila ila0(
+    .CONTROL(CONTROL0),
+    .CLK(USER_CLK),
+    .TRIG0({7'd0,USER_CLK}),
+    .TRIG1({7'd0,clk2}),
+    .TRIG2({7'd0,~clk179}),
+    .TRIG3({7'd0,~clk64}),
+    .TRIG4({7'd0,~clk16}),
+    .TRIG5(8'd0),
+    .TRIG6(8'd0),
+    .TRIG7(8'd0),
+    .TRIG8(8'd0),
+    .TRIG9(8'd0),
+    .TRIG10(8'd0),
+    .TRIG11(8'd0),
+    .TRIG12(8'd0),
+    .TRIG13(8'd0),
+    .TRIG14(8'd0),
+    .TRIG15(8'd0));
+    
+endmodule
+
+module clockGen(HALT,phi0_in,
+                RDY,phi1_out,phi2_out,phi1_extout,phi2_extout);
+                
+    input HALT,phi0_in;
+    output RDY;
+     (* clock_signal = "yes" *) output phi1_out,phi2_out,phi1_extout,phi2_extout;
+
+    wire phi0_buf;
+    reg haltEn;
+    //when disabled, phi0 is stuck at 0. which coincidentally is when phi1 stuck at 1.
+    //when enabled again, input should be already at 0 (phi1 tick just occurred), which merges nicely with the stuck at 0 phi.
+    //BUFGCE clockBuf(.O(phi0_buf),.I(phi0_in),.CE(~haltEn));
+    
+    BUFGCTRL #(
+       .INIT_OUT(0),           // Initial value of BUFGCTRL output ($VALUES;)
+       .PRESELECT_I0("TRUE"), // BUFGCTRL output uses I0 input ($VALUES;)
+       .PRESELECT_I1("FALSE")  // BUFGCTRL output uses I1 input ($VALUES;)
+    )
+    BUFGCTRL_inst (
+       .O(phi0_buf),             // 1-bit output: Clock output
+       .CE0(1'b1),         // 1-bit input: Clock enable input for I0
+       .CE1(1'b0),         // 1-bit input: Clock enable input for I1
+       .I0(phi0_in),           // 1-bit input: Primary clock
+       .I1(1'b0),           // 1-bit input: Secondary clock
+       .IGNORE0(1'b1), // 1-bit input: Clock ignore input for I0
+       .IGNORE1(1'b1), // 1-bit input: Clock ignore input for I1
+       .S0(~haltEn),           // 1-bit input: Clock select for I0
+       .S1(haltEn)            // 1-bit input: Clock select for I1
+    );
+   
+   
+    //LDCPE #(.INIT(1'b0)) clockLatch(.CLR(1'b0),.PRE(1'b0),.G(~haltEn),.GE(1'b1),.D(phi0_in),.Q(phi0_latch));
+    
+    //latch on phi1 ticks
+    always @ (negedge phi0_in) begin
+        haltEn <= HALT;
+    end
+    
+    BUFG a(phi1_out,~phi0_buf);
+    BUFG b(phi2_out,phi0_buf);
+    
+    BUFG c(phi1_extout,phi1_out);
+    BUFG d(phi2_extout,phi2_out);
+    
+endmodule
---
+++ POKEY/Audio/peripherals.v
@@ -0,0 +1,288 @@
+/****************************************************
+ * Project: Atari 5200                              *
+ *                                                  *
+ * Peripherals 			                            *
+ *                                                  *
+ * Team Atari                                       *
+ *    Alvin Goh     (chuehsig)                      *
+ *    Benjamin Hong (bhong)                         *
+ *    Jonathan Ong  (jonathao)                      *
+ ****************************************************/
+ 
+/* Changelog:
+	15 Sep 2014,  0033hrs: added memory module (chue)
+  29 Sep 2014,  1948hrs: modified memory module to become linear (ben)
+  30 Sep 2014,  1927hrs: amended memory module to be writable (ben)
+*/
+
+module triState(out,in,en);
+    inout out;
+    input in;
+    input en;
+    
+    assign out = (en) ? in : 1'bz;
+    
+endmodule
+
+module clockHalf(inClk,outClk);
+    input inClk;
+    output reg outClk = 1'b0;
+    
+    always @ (posedge inClk) begin
+        outClk <= ~outClk;
+    end
+    
+endmodule
+
+module clockone4(inClk,outClk);
+    input inClk;
+    output outClk;
+    
+    reg [1:0] count;
+    
+    always @ (posedge inClk) begin
+        count <= count + 1;
+    end
+    
+    assign outClk = count[1];
+    
+endmodule
+
+module clockone256(inClk,outClk);
+    input inClk;
+    output outClk;
+    
+    reg [7:0] count;
+    
+    always @ (posedge inClk) begin
+            count <= count + 1;
+    end
+    
+    assign outClk = count[7];
+endmodule
+
+module clockone1024(inClk,outClk);
+    input inClk;
+    output outClk;
+    
+    reg [9:0] count;
+    
+    always @ (posedge inClk) begin
+        count <= count + 1;
+    end
+    
+    assign outClk = count[9];
+endmodule
+
+module clockone2048(inClk,outClk);
+    input inClk;
+    output outClk;
+    
+    reg [10:0] count;
+    
+    always @ (posedge inClk) begin
+        count <= count + 1;
+    end
+    
+    assign outClk = count[10];
+endmodule
+
+module clockDivider(inClk,outClk);
+    parameter DIVIDE = 500;
+    
+function integer log2;
+    input [31:0] value;
+    for (log2=0; value>0; log2=log2+1)
+    value = value>>1;
+endfunction
+    
+    parameter width = log2(DIVIDE);
+        
+    input inClk;
+    output outClk;
+
+    
+    reg [12:0] counter = 0;
+    always @ (posedge inClk) begin
+        counter <= counter + 1;
+        if (counter == DIVIDE>>1) counter <= 0;
+    end
+    
+
+    wire en;
+    assign en = (counter == 0);
+
+    reg outClk = 1'b0;
+    
+    always @ (posedge inClk) begin
+            if (en) outClk <= ~outClk;
+            else outClk <= outClk;
+    end
+
+    
+endmodule
+
+
+// Assert 'en' to connect the left and right data buses
+module transBuf(en, leftDriver, rightDriver, left, right);
+
+    input en;
+    input [2:0] leftDriver,rightDriver;
+    inout [7:0] left, right;
+    
+    
+
+    
+    wire enLeft, enRight;
+    assign enLeft = (leftDriver > rightDriver);
+    assign enRight = (rightDriver > leftDriver);
+    
+    
+    
+    bufif1 LtoR[7:0](right, left, (en & enLeft));
+    bufif1 RtoL[7:0](left, right, (en & enRight));
+    //bufif1 LtoR[7:0](right, left, en);
+endmodule
+
+
+
+// 6502 can address up to 65K memory. (16 bits)
+module memory256x256 (clock, enable, we_L, re_L,address,
+				data);
+				
+	input clock, enable, we_L, re_L;
+	input [15:0] address;
+	inout [7:0] data;
+
+	wire clock, enable, we_L, re_L;
+	wire [15:0] address;
+	
+	// [7:0] addL, addH;
+	//assign {addH,addL} = address;
+    reg [7:0] data_reg;
+	
+	reg [7:0] mem [0:65535]; //65kb of linear memory declared
+  
+    //Initializing memory
+    initial begin
+     $readmemh("memory.list", mem);
+    end
+
+    //reading from memory is ASYNC
+    //writing to memory is SYNC
+	assign data = (enable & ~re_L) ? mem[address] : 8'bzzzzzzzz;
+    
+	always @(posedge clock) begin
+    
+		if (enable && ~we_L) begin
+      // $display("we're storing data at %h", address);
+			mem[address] = data;
+        end
+    /*
+    else if (~re_L) begin
+      // $display("we're reading data at %h", address);
+      data_reg <= mem[address]; //not sure if this screws up the timing for memory being accessible not.
+    end
+    else begin
+      $display("re_L is high, we_L is high");
+      data_reg <= 8'bxxxxxxxx;
+    end
+    */
+	end
+
+endmodule
+
+
+
+
+
+// DeBounce_v.v
+
+
+//////////////////////// Button Debounceer ///////////////////////////////////////
+//***********************************************************************
+// FileName: DeBounce_v.v
+// FPGA: MachXO2 7000HE
+// IDE: Diamond 2.0.1 
+//
+// HDL IS PROVIDED "AS IS." DIGI-KEY EXPRESSLY DISCLAIMS ANY
+// WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
+// BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
+// DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
+// PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+// BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+// ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+// DIGI-KEY ALSO DISCLAIMS ANY LIABILITY FOR PATENT OR COPYRIGHT
+// INFRINGEMENT.
+//
+// Version History
+// Version 1.0 04/11/2013 Tony Storey
+// Initial Public Release
+// Small Footprint Button Debouncer
+
+module  DeBounce 
+	(
+	input 			clk, n_reset, button_in,				// inputs
+	output reg 	DB_out													// output
+	);
+//// ---------------- internal constants --------------
+	parameter N = 11 ;		// (2^ (21-1) )/ 38 MHz = 32 ms debounce time
+////---------------- internal variables ---------------
+	reg  [N-1 : 0]	q_reg;							// timing regs
+	reg  [N-1 : 0]	q_next;
+	reg DFF1, DFF2;									// input flip-flops
+	wire q_add;											// control flags
+	wire q_reset;
+//// ------------------------------------------------------
+
+////contenious assignment for counter control
+	assign q_reset = (DFF1  ^ DFF2);		// xor input flip flops to look for level chage to reset counter
+	assign  q_add = ~(q_reg[N-1]);			// add to counter when q_reg msb is equal to 0
+	
+//// combo counter to manage q_next	
+	always @ ( q_reset, q_add, q_reg)
+		begin
+			case( {q_reset , q_add})
+				2'b00 :
+						q_next <= q_reg;
+				2'b01 :
+						q_next <= q_reg + 1;
+				default :
+						q_next <= { N {1'b0} };
+			endcase 	
+		end
+	
+//// Flip flop inputs and q_reg update
+	always @ ( posedge clk )
+		begin
+			if(n_reset ==  1'b0)
+				begin
+					DFF1 <= 1'b0;
+					DFF2 <= 1'b0;
+					q_reg <= { N {1'b0} };
+				end
+			else
+				begin
+					DFF1 <= button_in;
+					DFF2 <= DFF1;
+					q_reg <= q_next;
+				end
+		end
+	
+//// counter control
+	always @ ( posedge clk )
+		begin
+			if(q_reg[N-1] == 1'b1)
+					DB_out <= DFF2;
+			else
+					DB_out <= DB_out;
+		end
+
+endmodule
+
+
+
+
+
